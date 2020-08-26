e1e040a3fa021f9f91037972185dfa29139d3d19
can <alexeychub@mail.ru>
2020-08-26 16:27:28 +0300

    custom for comprehension

--- src/test/scala/ru/otus/temp/collectionsLecture.scala
+++ src/test/scala/ru/otus/temp/collectionsLecture.scala
@@ -23,5 +23,41 @@ class collectionsLecture extends AnyFreeSpec {
 
       filterFM(List(1, 2, 3, 4), x => (x % 2) == 0) shouldBe List(2, 4)
     }
+
+    "custom for comprehension" in {
+      sealed trait MaybeInt {
+        def map(f: Int => Int): MaybeInt
+        def flatMap(f: Int => MaybeInt): MaybeInt
+      }
+
+      case class SomeInt(i: Int) extends MaybeInt {
+        def map(f: Int => Int): MaybeInt = {
+          val fcall = f(i)
+          println(s"map on $i called with result $fcall")
+          SomeInt(fcall)
+        }
+        def flatMap(f: Int => MaybeInt): MaybeInt = {
+          val fcall = f(i)
+          println(s"flatMap on $i called with result $fcall")
+          fcall
+        }
+      }
+
+      val maybe1 = SomeInt(1)
+      val maybe2 = SomeInt(2)
+
+      val result = for {
+        a <- maybe1
+        b <- maybe2
+      } yield a + b
+
+//      val result: MaybeInt = maybe1
+//        .flatMap((a: Int) =>
+//          maybe2
+//            .map((b: Int) => a.+(b))
+//        )
+
+      result.shouldBe(SomeInt(3))
+    }
   }
 }
